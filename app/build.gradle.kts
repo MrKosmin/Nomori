0099dc56352c7a6784e4e75c29ccae56ee255cc0
LowMemory <muhailokosmin@gmail.com>
2025-07-05 13:23:05 +0300

    Додав swipeRefreshLayout у: Список серій Список плейлистів Історію

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,4 +83,5 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 }
